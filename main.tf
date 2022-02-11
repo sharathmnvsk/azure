cdeefb7f477a7b5405f08329c4e302fe1eda119d
sharathmnvsk <78378274+sharathmnvsk@users.noreply.github.com>
2022-02-11 17:12:43 -0500

    Added terraform storage files

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.storage_container_name
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name    = "storage_terraform_test"
+location               = "East US"
+storage_account_name   = "storageaccterraform1234"
+storage_container_name = "my-files"     
